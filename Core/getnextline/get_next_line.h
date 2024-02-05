48247d8a439158adb8437b1876d46e7a0205c8eb
Lukas Haken <lhaken@c2r6s1.42prague.com>
2024-02-05 12:15:53 +0100

    getnextline - adding headers and get_next_line prototype into .h file

--- Core/getnextline/get_next_line.h
+++ Core/getnextline/get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: lhaken <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 10:37:28 by lhaken            #+#    #+#             */
-/*   Updated: 2024/02/03 10:37:32 by lhaken           ###   ########.fr       */
+/*   Updated: 2024/02/05 11:36:14 by lhaken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+/* INCLUDES */
+# include <stdlib.h>
+# include <unistd.h>
+
+/* GNL */
+char	*get_nexz_line(int fd);
+
+#endif
